c1a219e0cd66df016d1b7c9a225a05006bf7cf04
hemendrta <hemendra.singh.c@outlook.com>
2021-01-21 20:36:09 -0800

    thymeleaf basics, variables declaration, loops, fetching values

--- bootrestemployee/src/main/java/com/api/employee/bootrestemployee/BootrestemployeeApplication.java
+++ bootrestemployee/src/main/java/com/api/employee/bootrestemployee/BootrestemployeeApplication.java
@@ -1,13 +1,32 @@
 package com.api.employee.bootrestemployee;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
+@Controller
 public class BootrestemployeeApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BootrestemployeeApplication.class, args);
+	}
+
+	@GetMapping("/")
+	public String getHomePage(Model model){
+		String str="hemendra singh chouhan";
+		
+		List<String> names=new ArrayList<String>();
+		names.addAll(List.of("Hemendra Singh Chouhan", "Aditi Gupta Chouhan", "Nisha Singh Chouhan", "Subhash Singh Chouhan"));
+		model.addAttribute("names", names);
+		model.addAttribute("name", str);
+		return "index";
+
 	}
 
 }
